3168264fd65139e89457b7047c53f5c3de761894
martingalli <martin.galli@wunderman.com>
2018-03-26 15:24:03 -0300

    Using PureComponent

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import Person from './Person/Person';
 
-class Persons extends Component {
+class Persons extends PureComponent {
     constructor(props) {
         super(props);
         console.log('[Persons.js] Inside Constructor', props);
@@ -13,6 +13,25 @@ class Persons extends Component {
     
     componentDidMount() {
         console.log('[Persons.js] Inside componentDidMount()');
+    }
+
+    componentWillReceiveProps (nextProps) {
+        console.log('[UPDATE Persons.js] Inside componentWillReceiveProps', nextProps);
+    }
+
+    // shouldComponentUpdate (nextProps, nextState) {
+    //     console.log('[UPDATE Persons.js] Inside shouldComponentUpdate', nextProps, nextState);
+    //     return nextProps.persons !== this.props.persons ||
+    //     nextProps.changed !== this.props.changed ||
+    //     nextProps.clicked !== this.props.clicked;
+    // }
+
+    componentWillUpdate (nextProps, nextState) {
+        console.log('[UPDATE Persons.js] Inside componentWillUpdate', nextProps, nextState);
+    }
+
+    componentDidUpdate () {
+        console.log('[UPDATE Persons.js] Inside componentDidUpdate');
     }
       
     render () {
