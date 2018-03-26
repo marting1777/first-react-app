76e085ce7ce00acb71447af5ccf1b9718f2b1434
martingalli <martin.galli@wunderman.com>
2018-03-26 16:22:26 -0300

    Usando High Order Functions

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
 import classes from './Person.css';
+import WithClass from '../../../hoc/WithClass';
 
 class Person extends Component {
+	constructor(props) {
+		super(props);
+		console.log('[Person.js] Inside Constructor', props);
+	}
+
+	componentWillMount () {
+		console.log('[Person.js] Inside componentWillMount()');
+	}
+
+	componentDidMount() {
+        console.log('[Persons.js] Inside componentDidMount()');
+    }
+
 	render () {
 		return (
-			<div className={classes.Person}>
+			<WithClass classes={classes.Person}>
 				<p onClick={this.props.click} style={{'padding': '10px'}}>Mi nombre es {this.props.name} y tengo {this.props.age} años.</p>
 				<p>{this.props.children}</p>
 				<input type="text" onChange={this.props.changed} value={this.props.name}/>
-			</div>
+			</WithClass>
 		);
 	}
 }
